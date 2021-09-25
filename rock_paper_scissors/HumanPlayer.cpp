d4ef0852b3311ced78e327d1beb3ef9a3e91c2c5
Saima Yunus <saimayunus925@gmail.com>
2021-09-24 18:47:50 -0700

    added basic 'human player choice' function, made .gitignore for Visual Studio boilerplate stuff

--- rock_paper_scissors/HumanPlayer.cpp
+++ rock_paper_scissors/HumanPlayer.cpp
@@ -1,7 +1,16 @@
+
 #include <HumanPlayer.hpp>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
 
 Choice HumanPlayer::makeChoice() {
-    
+    // Prompt the human player for each round’s choice of rock, paper, or scissors.
+    // 'Choice' enum: ROCK = 0, PAPER = 1, SCISSORS = 2
+    int c = 0;
+    std::cout << "Enter your choice as a number (ROCK = 0, PAPER = 1, SCISSORS = 2) here: " << std::endl;
+    std::cin >> c;
+    return Choice(c);
 }
 
 string HumanPlayer::getName() {
